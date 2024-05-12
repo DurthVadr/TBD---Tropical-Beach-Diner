abeb50faae24130e10294cae31dd1c4118fbf6c6
Mertcan Sağlam <mertcan.saglam@ozu.edu.tr>
2024-05-13 00:51:56 +0300

    Some functions initialized according to SDD. need to update issues accordingly.

---
+++ src/Restaurant/Inventory.java
@@ -0,0 +1,23 @@
+
+package Restaurant;
+
+import java.lang.reflect.Array;
+
+public class Inventory {
+    ArrayList<Item> items = new ArrayList<Item>();
+
+
+    public Inventory(ArrayList<Item> items) {
+        this.items = items;
+    }
+    
+    Item buyItem(Item item) {
+
+        return item;
+    }
+
+    Item takeItem(Item item) {
+
+        return item;
+    }
+}
--- src/Restaurant/Item.java
+++ src/Restaurant/Item.java
@@ -1,4 +1,20 @@
 package Restaurant;
 
 public class Item {
+    String type;
+    Integer amount;
+
+
+    public Item(String type, Integer amount) {
+        this.type = type;
+        this.amount = amount;
+    }
+
+    changeAmount(Integer n) {
+         //TODO: Implement
+        
+        n = amount;
+        return amount;
+    }
+
 }
