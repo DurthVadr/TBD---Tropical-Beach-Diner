cc17b2df151cde99cd4c46abe59684dff3f38bd3
Ata Seyhan <ata.seyhan@ozu.edu.tr>
2024-05-16 20:29:53 +0300

    Sample outline added in main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Test");
+
+        Customer customer = new Customer("Cemil", 1.0f); //initial satisfaction can be 1 or 0 ?
+
+        Item burger = new Item("Big Mac", 1);
+        Item fries = new Item("Large Fries", 1);
+        Item coke = new Item("Coke", 2);
+
+        List<Item> itemsOrdered = new ArrayList<>();
+        itemsOrdered.add(burger);
+        itemsOrdered.add(fries);
+        itemsOrdered.add(coke);
+
+        System.out.println("Customer Name: " + customer.getName());
     }
 }
