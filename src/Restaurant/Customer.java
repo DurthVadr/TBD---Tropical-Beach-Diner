c98036d3549131d0245cf5463449b4ddc5b5a6bc
Ata Seyhan <ata.seyhan@ozu.edu.tr>
2024-05-16 19:15:32 +0300

    Customer class functions implemented

--- src/Restaurant/Customer.java
+++ src/Restaurant/Customer.java
@@ -2,18 +2,50 @@ package Restaurant;
 
 public class Customer {
 
-    String name;
-    Float satisfaction;
+   private String name;
+    private Float satisfaction;
 
 
     public Customer(String name, Float satisfaction) {
         this.name = name;
         this.satisfaction = satisfaction;
     }
+    public float checkOrder(Item item, Order order) {
+        boolean itemFound = false;
+        float total = 0.0f;
 
+        for (Item orderedItem : order.getItems()) {
+            if (orderedItem.equals(item)) {
+                itemFound = true;
+                total += orderedItem.getPrice();
+            }
+        }
+        if (!itemFound) {
+            // Decrease satisfaction if the item not found
+            this.satisfaction -= 0.1; // Decrease by 10% (can be modified)
+            if (this.satisfaction < 0) {
+                this.satisfaction = 0;
+            }
+        }
+    }
 
     void giveOrder(Order order) {
         //TODO: Implement
         
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getSatisfaction() {
+        return satisfaction;
+    }
+
+    public void setSatisfaction(float satisfaction) {
+        this.satisfaction = satisfaction;
     }
 }
