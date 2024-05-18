b793b4f784185702de8b6034ce01738c8117fda3
Ata Seyhan <ata.seyhan@ozu.edu.tr>
2024-05-18 21:08:29 +0300

    Stocks initialized

---
+++ src/Persistence/Stocks.java
@@ -0,0 +1,14 @@
+package Persistence;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+public class Stocks {
+
+    private JButton viewStockButton;
+    private JButton closeStockButton;
+    private List<JButton> stockPageButtons;
+    private List<JLabel> stocksLabel;
+}
